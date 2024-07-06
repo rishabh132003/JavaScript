a982ec5bd8b3496cdbd9fbb8e5f6bedc25dbe2da
rishabh132003 <103423564+rishabh132003@users.noreply.github.com>
2024-07-06 12:19:56 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat al JS code as newer version
+
+// alert(3+3) // we are using nodeJS not browser
+
+let name = "Rishabh"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol
+
+// object
+
+console.log(typeof "fhfgh");
+console.log(typeof age);
+console.log(typeof null); // Object 
+console.log(typeof undefined); // undefined
